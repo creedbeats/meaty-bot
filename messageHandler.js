1211a98d5edf919e081a3412ffb3420c74ae1cd2
Chris Reed <chris@reed.zone>
2019-02-25 19:06:14 -0500

    added region request feature

--- messageHandler.js
+++ messageHandler.js
@@ -20,17 +20,21 @@ class MessageHandler {
   }
   
   roleRequest(msg) {
-    const allRoles = msg.guild.roles;
-    const re = /^!([^\s]+)\s+(.+)$/ig;
-    const match = re.exec(msg.content);
+    const needsHelp = /^!help/i.test(msg.content);
+    if (needsHelp) {
+      msg.reply(`Try using one of the following commands to make your request \`!character\` or \`!region\``)
+      return;
+    }
 
+    const allRoles = msg.guild.roles;
+    const re = /^!([^\s]+)\s*(.+)$/ig;
+    const match = re.exec(msg.content);
     if (!match) return;
 
     const reqType = match[1].toLowerCase();
-    const req = match[2] ? match[2].split(',').map(s => s.trim()) : null;
+    const req = match[2] ? match[2].split(',').map(s => s.trim()) : [];
     
     if (typeof roleRequests[reqType] !== 'function') return;
-    
     roleRequests[reqType]({ msg, req, allRoles });
   }
 }
