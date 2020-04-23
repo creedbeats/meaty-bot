5576f364912a66e0baba4cfe3497e5a9dba1842f
Chris Reed <creeddesigns@gmail.com>
2020-04-23 18:43:41 -0400

    updated env variable name to match documentation"

--- index.js
+++ index.js
@@ -7,15 +7,21 @@ require('dotenv').config();
 client.on('ready', () => console.log('Bot is now connected'));
 
 client.on('guildMemberAdd', (member) => {
-  console.log(`New User "${member.user.username}" has joined "${member.guild.name}"`);
-  member.guild.channels.find('name', 'general').send(`"${member.user.username}" has joined this server`);
+  console.log(
+    `New User "${member.user.username}" has joined "${member.guild.name}"`
+  );
+  member.guild.channels
+    .find('name', 'general')
+    .send(`"${member.user.username}" has joined this server`);
 });
 
 client.on('guildMemberRemove', (member) => {
   console.log(`User "${member.user.username}" has left "${member.guild.name}"`);
-  member.guild.channels.find('name', 'general').send(`"${member.user.username}" has left this server`);
+  member.guild.channels
+    .find('name', 'general')
+    .send(`"${member.user.username}" has left this server`);
 });
 
 messageHandler.init();
 
-client.login(process.env.CLIENT_TOKEN);
+client.login(process.env.token);
